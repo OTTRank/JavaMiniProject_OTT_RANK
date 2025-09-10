3c4928bb5cec12389d86600c1fccc4f049ae32e9
sist <sist@sist27>
2025-09-10 13:56:46 +0900

    22

--- OTT_RANK/src/com/sist/client/Login.java
+++ OTT_RANK/src/com/sist/client/Login.java
@@ -2,46 +2,45 @@ package com.sist.client;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-
-public class Login extends JFrame {
+public class Login extends JFrame 
+implements ActionListener
+{
     JLabel la1, la2;
     JTextField tf;
     JPasswordField pf;
     JButton b1, b2, b3;
 
+    JoinForm join=new JoinForm();
+    
     public Login() {
-    	 la1 = new JLabel("ID", JLabel.CENTER);
-         la2 = new JLabel("PW", JLabel.CENTER);
-         tf = new JTextField();
-         pf = new JPasswordField();
-         b1 = new JButton("로그인");
-         b2 = new JButton("가입");
-         b3 = new JButton("취소");
-         
-         
+       
         setTitle("Login");
-        setSize(250, 200); 
+        setSize(253, 175); 
         setLayout(null);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE); // EXIT_ON_CLOSE면 로그인 창 닫았을 시 메인창도 같이 꺼짐
         setLocationRelativeTo(null); 
 
-        
-     
-        
-        //배치
-        setLayout(null);
-        la1.setBounds(10, 20, 30, 20);
-        tf.setBounds(45, 20, 180, 20);
+        la1 = new JLabel("ID", JLabel.CENTER);
+        la2 = new JLabel("PW", JLabel.CENTER);
+        tf = new JTextField();
+        pf = new JPasswordField();
+        b1 = new JButton("로그인");
+        b2 = new JButton("가입");
+        b3 = new JButton("취소");
 
-        la2.setBounds(10, 50, 30, 20);
-        pf.setBounds(45, 50, 180, 20);
+       
+        la1.setBounds(10, 18, 40, 25);
+        tf.setBounds(57, 18, 118, 28);
 
-        JPanel p=new JPanel();
+        la2.setBounds(14, 51, 40, 25);
+        pf.setBounds(57, 51, 118, 28);
+
+        JPanel p = new JPanel();
         p.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 0)); 
         p.add(b1);
         p.add(b2);
         p.add(b3);
-        p.setBounds(10, 85, 215, 30);
+        p.setBounds(15, 93, 205, 30);
 
         
         add(la1);
@@ -49,29 +48,31 @@ public class Login extends JFrame {
         add(la2);
         add(pf);
         add(p);
-    
-        b3.addActionListener(new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            dispose();  //취소
-        }
-      });
-        b2.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                dispose();  //창에 뭐 안쳐도 로그인되는 거
-            }
-        }); 
         
-        b1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                dispose();  //창에 뭐 안쳐도 로그인되는 거
-            }
-        }); 
+        b1.addActionListener(this); // 로그인
+       b2.addActionListener(this); // 회원가입 
+       b3.addActionListener(this); // 취소
     }
-    
+
     public static void main(String[] args) {
         new Login().setVisible(true);
     }
+
+   @Override
+   public void actionPerformed(ActionEvent e) {
+      if(e.getSource()==b1)
+      {
+         dispose();
+      }
+      else if(e.getSource()==b2)
+      {
+         this.setVisible(false);
+         join.setVisible(true);
+      }
+      else if(e.getSource()==b3)
+      {
+         dispose();
+         System.exit(0);
+      }
+   }
 }
